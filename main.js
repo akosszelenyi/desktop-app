b52b78a42937bdde921b5e43962675e72ddaaad3
Akos Szelenyi <a1103546@unet.univie.ac.at>
2018-05-14 16:24:21 +0200

    Initial version

--- main.js
+++ main.js
@@ -4,12 +4,25 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
+const { autoUpdater, dialog } = require('electron')
+
+
+
 const path = require('path')
 const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+require('update-electron-app')()
+
+const server = 'https://hazel-server-rlwssvwkwd.now.sh'
+  const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+  autoUpdater.setFeedURL(feed)
+
+
 
 function createWindow () {
   // Create the browser window.
@@ -55,6 +68,25 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+
+    dialog.showMessageBox(dialogOpts, (response) => {
+      if (response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
